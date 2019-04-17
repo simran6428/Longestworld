8126b8198d55b609901f61f3e2c6c74ecdeaf0e2
visualstudio <visualstudio@122-PC20>
2019-04-17 10:12:02 -0400

    world

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace LongestWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest world is :");
+            Console.WriteLine(a.MyFunctionA("ada lovelace wrote the first algorith desingned for processing bt an Analytical Engine."));
+        }
+
+        public MyFunctionA(string input)
+        {
+            // Write a C# prograam to find the longest word in a string.
+            string[] words = input.Split('');
+
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+            int x = 0;
         }
     }
 }
